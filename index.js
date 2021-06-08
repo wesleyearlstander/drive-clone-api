170b13c5cff924f451eccda651f6e65acd5f8c4c
Wesley Earl Stander <wesleys@bbd.co.za>
2021-06-08 20:00:34 +0200

    using bearer tokens

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
 const express = require("express");
 const path = require("path");
 const { auth, requiresAuth } = require('express-openid-connect');
+var jwt = require('express-jwt');
+var jwks = require('jwks-rsa');
 
 var os = require('os');
 const app = express();
 const port = process.env.PORT || 8000;
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://cmt-dev.eu.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'https://drive-clone-api.herokuapp.com/',
+  issuer: 'https://cmt-dev.eu.auth0.com/',
+  algorithms: ['RS256']
+});
 
-const config = {
-    authRequired: false,
-    auth0Logout: true,
-    secret: 'a long, randomly-generated string stored in env',
-    baseURL: (os.hostname().indexOf("local") > -1) ? 'http://localhost:8000' : "https://drive-clone-api.herokuapp.com",
-    clientID: '5yUSJVHVOXqHWd2rZoaqTDYZGACxFnGP',
-    issuerBaseURL: 'https://cmt-dev.eu.auth0.com'
-};
-
-app.use(auth(config));
+app.use(jwtCheck);
 
 // app.get("/", (req, res) => {
 //     res.status(200).send("Found the API");
 // });
 
-app.get('/callback', (req, res) => {
-    res.redirect("/");
-});
-
-app.get('/logout', (req, res) => {
-    req.logout();
-    res.redirect("/");
-});
-
 app.get('/profile', requiresAuth(), (req, res) => {
     res.send(JSON.stringify(req.oidc.user));
-  });
+});
 
 // req.isAuthenticated is provided from the auth router
 app.get('/', (req, res) => {
-    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+    res.send("You found our api");
 });
 
 app.listen(port, () => {
