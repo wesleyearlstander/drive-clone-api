c85890ca48c443e28a6cf7669b3644a1c713dc15
Shaikjee, Mohammed, Vodacom South Africa (External) <Mohammed.Shaikjee@vcontractor.co.za>
2021-06-08 20:13:07 +0200

    #1: swagger api-docs initial implementation

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const express = require("express");
 const cors = require("cors");
 const { auth } = require('express-openid-connect');
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 const authRouter = require("./routes/auth");
 const os = require('os');
+const request = require("request");
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const options = {
+    method: 'POST',
+    url: 'https://cmt-dev.eu.auth0.com/oauth/token',
+    headers: {
+        'content-type': 'application/json'
+    },
+    body: `{"client_id":"${process.env.CLIENT_ID}","client_secret":"${process.env.CLIENT_SECRET}","audience":"https://drive-clone-api.herokuapp.com/","grant_type":"client_credentials"}`
+};
+
+request(options, function (error, response, body) { if (error) throw new Error(error); console.log(body); });
+
+const port = process.env.port || "8000";
+
+const swaggerOptions = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:8000",
+			},
+		],
+	},
+	apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(swaggerOptions);
 
 const app = express();
-const port = process.env.port || "8000";
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 const config = {
     authRequired: false,
