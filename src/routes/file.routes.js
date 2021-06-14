f8cd8b69bdc47ffedcca3470564e183098b8c4de
Governor Baloyi <Governor.Baloyi@vcontractor.co.za>
2021-06-14 17:39:57 +0200

    FET-01: Use status codes, send json response, mock the file route

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -1,24 +1,67 @@
-const express = require('express');
+const express = require("express");
 const fileRouter = express.Router();
-const { upload } = require('../controllers/file.controller');
+const { upload } = require("../controllers/file.controller");
+const { StatusCodes } = require("http-status-codes");
 
 // TODO: Mohammed
-fileRouter.post('/upload/file', upload);
+fileRouter.post("/upload/file", upload);
 
 // TODO: Mohammed
-fileRouter.get('/download', (req, res) => {
+fileRouter.get("/download", (req, res) => {});
+
+fileRouter.put("/move", (req, res) => {
+  const file = req.query.file;
+  const directory = req.query.directory;
+
+  if (!file) {
+    res.status = StatusCodes.NOT_FOUND;
+    res.json({
+      message: "File Not Found",
+    });
+  }
+
+  res.status = StatusCodes.OK;
+  res.json({
+    message: `${file} has been moved to ${directory}`,
+  });
 });
 
-// TODO: Governor
-fileRouter.put('/move', (req, res) => {
+fileRouter.patch("/rename", (req, res) => {
+  const file = req.query.file;
+  const name = req.query.name;
+
+  if (!file) {
+    res.status = StatusCodes.NOT_FOUND;
+    res.json({
+      message: `File: ${file} not found`,
+    });
+
+    if (!name) {
+      res.status = StatusCodes.BAD_REQUEST;
+      res.json({
+        message: 'Please provide new file name',
+      });
+    }
+  }
+
+  res.status = StatusCodes.OK;
+  res.json({
+    message: `File ${file} renamed to ${name}`
+  });
 });
 
-// TODO: Governor
-fileRouter.patch('/rename', (req, res) => {
-});
-
-// TODO: Governor
-fileRouter.delete('/delete', (req, res) => {
-});
+fileRouter.delete("/delete", (req, res) => {
+  const file = req.query.file;
+  
+  if (!file) {
+    res.status = StatusCodes.NOT_FOUND;
+    res.json({
+      message: `File: ${file} not found`
+    });
+  }
+  
+  res.status = StatusCodes.NO_CONTENT;
+  res.send();
+})
 
 module.exports = fileRouter;
