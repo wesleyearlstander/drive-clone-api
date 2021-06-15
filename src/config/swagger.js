e7a3f2e65558e38d408ddf130a398eda1df0fdfe
Wesley Earl Stander <wesleys@bbd.co.za>
2021-06-15 18:44:40 +0200

    updated version

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -9,15 +9,6 @@ const swaggerOptions = {
       description: 'A simple Express Library API',
     },
     basePath: '/',
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        }
-      }
-    },
     servers: [
       {
           url: `http://localhost:${process.env.PORT || 8000}`
@@ -26,30 +17,16 @@ const swaggerOptions = {
           url: 'https://drive-clone-api.herokuapp.com',
       },
     ],
-      security: [
-        {
-          auth0: [
-            'openid',
-            'admin:org',
-            'admin:public_key'
-          ]
-        }
-      ],
-      securityDefinitions: {
-        auth0: {
-          type: 'oauth2',
-          scopes: {
-            openid: 'Grants access to user_id',
-            'admin:org': 'Fully manage organization, teams, and memberships.',
-            'admin:public_key': 'Fully manage public keys.'
-          },
-          flow: 'accessCode',
-          authorizationUrl: 'https://test.eu.auth0.com/authorize',
-          tokenUrl: 'https://test.eu.auth0.com/userinfo',
-          'x-token-validation-url': 'https://test.eu.auth0.com/userinfo'
-        }
-      },
-    },
+    security: [
+      {
+        auth0: [
+          'openid',
+          'admin:org',
+          'admin:public_key'
+        ]
+      }
+    ]
+  },
   apis: ['./src/routes/*.js'],
 };
 
